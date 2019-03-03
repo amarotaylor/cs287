02ca118322daf23d65ab2c936a51088553a27c56
amarotaylor <amaro@broadinstitute.com>
2019-03-03 18:10:02 +0000

    working seq2seq model!

--- hw3/common.py
+++ hw3/common.py
@@ -7,16 +7,15 @@ from namedtensor.text import NamedField
 import numpy as np
 
 class SequenceModel(nn.Module):
-    def __init__(self, src_vocab_size, context_size, num_layers=2, weight_init=0.08):
+    def __init__(self, src_vocab_size, context_size, num_layers=2, weight_init=0.08, dropout=0.4):
         super(SequenceModel, self).__init__()
         self.context_size = context_size
         # embedding
         self.embedding = nn.Embedding(src_vocab_size, context_size)
         # langauge summarization
-        self.lstm = nn.LSTM(input_size=context_size, hidden_size=context_size, num_layers=num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size=context_size, hidden_size=context_size, num_layers=num_layers, batch_first=True, dropout=dropout)
         #for p in self.lstm.parameters():
         #    torch.nn.init.uniform_(p, a=weight_init, b=weight_init)
-
     def forward(self, inputs, h0=None):
         # embed the words 
         embedded = self.embedding(inputs)
@@ -25,7 +24,7 @@ class SequenceModel(nn.Module):
         return context, hidden
     
 class LanguageModel(nn.Module):
-    def __init__(self, target_vocab_size, hidden_size, context_size, weight_init = 0.08):
+    def __init__(self, target_vocab_size, hidden_size, context_size, weight_init = 0.08,dropout=0.4):
         super(LanguageModel, self).__init__()
         # context is batch_size x seq_len x context_size
         # context to hidden
@@ -41,17 +40,39 @@ class LanguageModel(nn.Module):
     def forward(self, inputs, h0=None):
         # embed the trg words
         embedded = self.embedding(inputs)
-        embedded = torch.nn.functional.relu(embedded)
         # setting hidden state to context at t=0
         # otherwise context = prev hidden state
         output, hidden = self.lstm(embedded, h0)
         output = self.translate(output)
         return output,hidden
+    
+    
+    
+# LSTM RNN
+class RNNet(torch.nn.Module):
+
+    def __init__(self, input_size, hidden_size, num_layers, dropout=0.5, weight_tie=False, weight_init=0.05):
+        super(RNNet, self).__init__()
+        self.emb = torch.nn.Sequential(torch.nn.Embedding(input_size, hidden_size), torch.nn.Dropout(dropout))
+        self.rnn = torch.nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, num_layers=num_layers, bias=True, batch_first=True, dropout=dropout)
+        self.lnr = torch.nn.Sequential(torch.nn.Dropout(dropout), torch.nn.Linear(hidden_size, input_size))
+    
+        for f in self.parameters():
+            torch.nn.init.uniform_(f, a=-weight_init, b=weight_init)
+      
+        if weight_tie == True:
+            self.lnr[1].weight.data=self.emb[0].weight.data
+      
+    def forward(self, inputs, h0=None):
+        x = self.emb(inputs) # batch x seqlen x hidden
+        x, hidden = self.rnn(x, h0) # batch x seqlen x hidden
+        y = self.lnr(x) # batch x seqlen x vocab
+        return y, hidden    
+    
+    
 
 def repackage_hidden(h):
     return tuple(v.detach() for v in h)
-def repackage_layer(hidden_s2c,hidden=100,BATCH_SIZE=32):
-    return tuple([hidden_s2c[0][-1].detach().view(1,BATCH_SIZE,hidden),hidden_s2c[1][-1].detach().view(1,BATCH_SIZE,hidden)])
 def reverse_sequence(src):
     length = list(src.shape)[1]
     idx = torch.linspace(length-1, 0, steps=length).long()
@@ -59,7 +80,7 @@ def reverse_sequence(src):
     return rev_src
 
 lsm = torch.nn.LogSoftmax(dim=2)
-criterion = nn.CrossEntropyLoss(reduction='none')
+criterion = nn.CrossEntropyLoss(reduction='sum')
 
 def training_loop(e,train_iter,seq2context,context2trg,seq2context_optimizer,context2trg_optimizer,BATCH_SIZE):
     seq2context.train()
@@ -77,12 +98,11 @@ def training_loop(e,train_iter,seq2context,context2trg,seq2context_optimizer,con
             break
         else:
             # generate hidden state for decoder
-            context, hidden_s2c = seq2context(src,h0)
-            hidden = repackage_layer(hidden_s2c,context_size,BATCH_SIZE)
-            output, hidden_lm = context2trg(trg[:,:-1],hidden)
+            context, hidden_s2c = seq2context(src)
+            output, hidden_lm = context2trg(trg[:,:-1],hidden_s2c)
             loss = criterion(output.transpose(2,1),trg[:,1:])
-            mask = trg[:,1:]!=1
-            loss = loss[mask].sum()
+            #mask = trg[:,1:]!=1
+            #loss = loss[mask].sum()
             #clip_grad_norm_(seq2context.parameters(), max_norm=5)
             #clip_grad_norm_(context2trg.parameters(), max_norm=5)
             loss.backward()
@@ -108,8 +128,9 @@ def validation_loop(e,val_iter,seq2context,context2trg,seq2context_sch,context2t
         else:
             # generate hidden state for decoder
             context, hidden_s2c = seq2context(src,h0)
-            hidden = repackage_layer(hidden_s2c,context_size)
-            output, hidden_lm = context2trg(trg[:,:-1],hidden)
+            #hidden = repackage_layer(hidden_s2c,context_size)
+            last_hidden = tuple([hidden_s2c[0][-1].unsqueeze(0),hidden_s2c[1][-1].unsqueeze(0)])
+            output, hidden_lm = context2trg(trg[:,:-1],last_hidden)
             loss = criterion(output.transpose(2,1),trg[:,1:])
             mask = trg[:,1:]!=1
             loss = loss[mask].detach().sum()
@@ -119,7 +140,6 @@ def validation_loop(e,val_iter,seq2context,context2trg,seq2context_sch,context2t
             total_loss += loss
             #track_mean += mean_loss
             total_words += mask.sum().float()
-    #ppl = torch.exp(track_mean/torch.tensor(ix).float())
     ppl = torch.exp(total_loss/total_words)
     seq2context_sch.step(ppl)
     context2trg_sch.step(ppl)
